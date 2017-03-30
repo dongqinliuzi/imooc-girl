d49441cf589395df84b5fbf2388a6b9c8ce4d96a
tizi <heitizi@yiran.com>
2017-03-31 01:46:24 +0800

    fix 2-3 使用AOP处理请求

--- src/main/java/com/imooc/aspect/HttpAspect.java
+++ src/main/java/com/imooc/aspect/HttpAspect.java
@@ -1,12 +1,21 @@
 package com.imooc.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 
 /**
@@ -24,14 +33,33 @@ public class HttpAspect {
     }
 
     @Before("log()")
-    public void before() {
-        System.out.println(1111111111);
-        logger.info("1212121");
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //url
+        logger.info("url={}", request.getRequestURI());
+        //method
+        logger.info("method={}", request.getMethod());
+        //ip
+        logger.info("ip={}", request.getRemoteAddr());
+        //类方法
+        logger.info("class-method={}", joinPoint.getSignature().getDeclaringTypeName() + " -- "
+        + joinPoint.getSignature().getName());
+        //参数
+        logger.info("args={}", joinPoint.getArgs());
+//        System.out.println(1111111111);
+//        logger.info("1212121");
     }
 
     @After("log()")
     public void doAfter() {
         System.out.println(1111111111);
         logger.info("343434");
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object) {
+        logger.info("response={}", object.toString());
     }
 }
